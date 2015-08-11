f40c03b1b2bd3081948d737e7d12f95f990c65ee
matthew-compton <compton@bignerdranch.com>
2015-08-11 17:56:19 -0400

    Added Frame Animation example

--- app/src/main/java/com/bignerdranch/android/lollipopanimations/FrameAnimationsActivity.java
+++ app/src/main/java/com/bignerdranch/android/lollipopanimations/FrameAnimationsActivity.java
@@ -2,13 +2,32 @@ package com.bignerdranch.android.lollipopanimations;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class FrameAnimationsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_frame_animations);
+
+        final ImageView frameAnimationImageView = (ImageView) findViewById(R.id.activity_frame_animations_imageview);
+        frameAnimationImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                frameAnimationImageView.setActivated(!frameAnimationImageView.isActivated());
+            }
+        });
+
+        final ViewGroup layout = (ViewGroup) findViewById(R.id.activity_frame_animations_layout);
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                frameAnimationImageView.setActivated(!frameAnimationImageView.isActivated());
+            }
+        });
     }
 
 }
